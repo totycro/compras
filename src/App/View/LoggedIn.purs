9c3d528d21ad21a95a435bb2b20fdbd48ae970cf
Bernhard Mallinger <b.mallinger@gmx.at>
2017-10-28 17:07:12 +0200

    Make detail view more beautiful

--- src/App/View/LoggedIn.purs
+++ src/App/View/LoggedIn.purs
@@ -5,14 +5,12 @@ import Prelude hiding (div)
 import App.Events (Event(..))
 import App.State
 import App.Types
-import Control.Bind (discard)
-import Data.Function (($))
-import Data.Maybe (fromMaybe, Maybe(..))
-import Data.List (List(..), (:), foldl)
+import Data.Maybe (Maybe(..))
+import Data.List (List)
 import Pux.DOM.HTML (HTML)
 import Pux.DOM.Events (onClick, onChange, onInput, targetValue)
-import Text.Smolder.HTML hiding (style)
-import Text.Smolder.HTML.Attributes hiding (span)
+import Text.Smolder.HTML
+import Text.Smolder.HTML.Attributes as Attributes
 import Text.Smolder.Markup ((#!), (!), text, class Attributable)
 import Data.Foldable (for_)
 
@@ -27,18 +25,20 @@ view (State st) =
   div do
     h1 $ text ( "Bienvenid@ " <> (getUserName st.currentUser))
     div do
-      button ! className "btn btn-primary" #! onClick (const RequestShoppingLists) $ text "load"
+      button ! Attributes.className "btn btn-primary" #! onClick (const RequestShoppingLists) $ text "load"
     centralView st.lists (getSelectedList (State st)) st.newItemName
     showAddNewList st.newListName
+
 
 centralView :: RemoteData GenericLoadingError (List ShoppingList) -> Maybe ShoppingList -> String -> HTML Event
 centralView shoppingLists (Just sl) newItemName = div do
    button
-     ! className "btn btn-secondary mb-3"
+     ! Attributes.className "btn btn-secondary mb-3"
      #! onClick (ShoppingListSelected Nothing)
      $ text "back"
    showSelectedList sl newItemName
 centralView shoppingLists Nothing _ = showLists shoppingLists
+
 
 -- TODO: datetime serialization
 -- TODO: button for actually deleting?
@@ -49,22 +49,18 @@ centralView shoppingLists Nothing _ = showLists shoppingLists
 showAddNewList :: String -> HTML Event
 showAddNewList newListName =
   div do
-    h5 ! style "margin-top: 32px" $ text "Add new list"
-    input ! value newListName #! onInput (\ev -> ChangeNewListName $ targetValue ev)
+    h5 ! Attributes.style "margin-top: 32px" $ text "Add new list"
+    input ! Attributes.value newListName #! onInput (\ev -> ChangeNewListName $ targetValue ev)
     (disableIfStringEmpty newListName button) #! onClick (const AddNewList) $ text "add"
 
 disableIfStringEmpty :: forall ta. Attributable ta => String -> ta -> ta
-disableIfStringEmpty "" x = x ! disabled "disabled"
+disableIfStringEmpty "" x = x ! Attributes.disabled "disabled"
 disableIfStringEmpty _ x = x
 
 
 createCheckbox :: Boolean -> HTML Event
-createCheckbox value = input ! type' "checkbox" ! checked (if value then "checked" else "" )
+createCheckbox value = input ! Attributes.type' "checkbox" ! Attributes.checked (if value then "checked" else "" )
 
-showItem :: Item -> HTML Event
-showItem (Item item) = li do
-  text item.name
-  createCheckbox item.bought #! onClick (const $ RequestToggleBoughtState item.id (not item.bought))
 
 showLists :: RemoteData GenericLoadingError (List ShoppingList) -> HTML Event
 showLists (Success listList) =
@@ -75,26 +71,44 @@ showLists NotAsked = span $ text "Lists not loading yet"
 showLists Loading = span $ text "Loading"
 showLists (Failure e) = span $ text $ "Error while loading:" <> show e
 
+
 showListInOverview :: ShoppingList -> HTML Event
 showListInOverview (ShoppingList shoppingList) =
   div do
     a
       -- ! style "cursor: pointer"
-      ! href "#"
+      ! Attributes.href "#"
       #! onClick (ShoppingListSelected $ Just $ ShoppingList shoppingList)
       $ text $ "list: " <> shoppingList.name
 
 
 showSelectedList :: ShoppingList -> String -> HTML Event
 showSelectedList (ShoppingList sl) newItemName = div do
-  showList (ShoppingList sl)
-  h5 $ text "Add new item to list"
-  input ! value newItemName #! onChange (\ev -> ChangeNewItemName $ targetValue ev)
-  (disableIfStringEmpty newItemName button ) #! onClick (const $ AddNewItem sl.id) $ text "add"
+  showList (ShoppingList sl) (div $ do
+    h5 $ text "Add new item to list"
+    input ! Attributes.value newItemName #! onChange (\ev -> ChangeNewItemName $ targetValue ev)
+    (disableIfStringEmpty newItemName button ) #! onClick (const $ AddNewItem sl.id) $ text "add"
+  )
 
-showList :: ShoppingList -> HTML Event
-showList (ShoppingList shoppingList) =
-  div ! className "card mb-3" $ do
-     div ! className "card-body" $ do
-       h3 ! className "card-title" $ text shoppingList.name
-       ul $ for_ shoppingList.items showItem
+
+showList :: ShoppingList -> HTML Event -> HTML Event
+showList (ShoppingList shoppingList) transclusion =
+  div ! Attributes.className "card mb-3" $ do
+     div ! Attributes.className "card-body" $ do
+       h3 ! Attributes.className "card-title" $ text shoppingList.name
+       ul ! Attributes.className "list-unstyled" $ for_ shoppingList.items showItem
+       transclusion
+
+
+showItem :: Item -> HTML Event
+showItem (Item item) = li do
+  -- using labels for the checkbox would be the most standard and accessible way,
+  -- however it screws up bootstrap styling. so we just make everything clickable here
+  div ! Attributes.className "input-group" #! onClick (const $ RequestToggleBoughtState item.id (not item.bought)) $ do
+    span ! Attributes.className "input-group-addon" $
+      createCheckbox item.bought
+    span
+      ! Attributes.className "form-control form-control-plaintext"
+      -- TODO: extract to proper css style class ("span with input-like border")
+      ! Attributes.style "color: black; border-color: rgb(206, 212, 218); border-style: solid ; border-width: 1px;"
+      $ text item.name
